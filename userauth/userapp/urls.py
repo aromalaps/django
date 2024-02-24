0ec874b13fb5b85e856213f471559454d37c82d6
aromalprasad <aromalaps@gmail.com>
2024-02-24 10:31:12 +0530

    user

--- userauth/userapp/urls.py
+++ userauth/userapp/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     
     path('',views.Home,name='home'),
     path('register/',views.Register,name='register'),
+    path('login/',views.login,name='Login'),
+    path('logout/',views.logout,name='Logout'),
+
 ]
--- userauth/userapp/views.py
+++ userauth/userapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django. contrib import auth
 
 # Create your views here.
 def Home(req):
@@ -14,14 +15,35 @@ def Register(req):
         cpassword = req.POST.get("cpassword",'')
         if password==cpassword:
            if User.objects.filter(email=email).exists():
-               print("email exsist")               
+               print("email exsist")  
+               return redirect('register')     
+           elif User.objects.filter(email=email).exists():
+                print(" email already exist")
+                return redirect('register')         
            else:
                user = User.objects.create_user(first_name=fname,last_name=lname,email=email,username=username,password=password)
                user.save()    
-               redirect('home')
+               redirect('Login')
         else:
             print('password not matching')
+            return redirect('register')
     return render(req,'register.html')
-def Login(req):
-    req
-    return()
+
+def login(req):
+    if req.method=='POST':
+        username=req.POST.get("username","")
+        password=req.POST.get("password","")
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+          auth.login(req,user)
+          print(user)
+          req.session['user']=str(user)
+          return redirect("home")
+        else:
+            print("invalid")
+            return redirect('Login')
+    return render(req,'login.html')
+def logout(req):
+    auth.logout(req)
+    req.session.pop('user',None)
+    return redirect('home')
